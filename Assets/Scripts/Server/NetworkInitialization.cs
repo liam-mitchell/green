5fc2348880500ad27359eeef9b07710320eaf12d
Liam Mitchell <ldmitche@ucalgary.ca>
2015-08-23 21:13:06 -0600

    Added manual client-side interpolation of transforms.

---
+++ Assets/Scripts/Server/NetworkInitialization.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkInitialization : NetworkBehaviour {
+	public float sendRate;
+
+	// Use this for initialization
+	void Start () {
+		if (isServer) {
+			Network.sendRate = sendRate;
+		}
+	}
+}
---
+++ Assets/Scripts/Shared/InterpolatedTransform.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[NetworkSettings(channel = 0, sendInterval = 0.05f)]
+public class InterpolatedTransform : NetworkBehaviour {
+	private class SyncTransform {
+		public Vector3 position;
+		public Quaternion rotation;
+		public float time;
+
+		public SyncTransform(Vector3 p, Quaternion r) {
+			position = p;
+			rotation = r;
+			time = Time.time;
+		}
+	}
+
+	private List<SyncTransform> transforms;
+	private float lastSync;
+
+	// Use this for initialization
+	void Start () {
+		lastSync = 0;
+	}
+
+	// Update is called once per frame
+	[ClientCallback]
+	void Update () {
+		if (transforms == null) {
+			return;
+		}
+
+		var renderDelay = 2 * (1.0f / Network.sendRate);
+		var renderTime = Time.time - renderDelay;
+
+		for (int i = 0; i < transforms.Count - 1; ++i) {
+			if (transforms[i + 1].time < renderTime) {
+				var last = transforms[i + 1];
+				var next = transforms[i];
+				var lerpTime = 1 - (next.time - renderTime) / (next.time - last.time);
+
+				transform.position = Vector3.Lerp (last.position, next.position, lerpTime);
+				transform.rotation = Quaternion.Lerp (last.rotation, next.rotation, lerpTime);
+				if (i < transforms.Count - 2) {
+					var trimPosition = i + 2;
+					transforms.RemoveRange (trimPosition, (transforms.Count - trimPosition));
+				}
+
+				break;
+			}
+		}
+
+	}
+
+	[ServerCallback]
+	void FixedUpdate() {
+		SetDirtyBit(1);
+	}
+
+	public override bool OnSerialize(NetworkWriter writer, bool initial) {
+		writer.Write (transform.position);
+		writer.Write (transform.rotation);
+		return true;
+	}
+
+	public override void OnDeserialize(NetworkReader reader, bool initial) {
+		var position = reader.ReadVector3 ();
+		var rotation = reader.ReadQuaternion();
+
+		if (transforms == null) {
+			transforms = new List<SyncTransform>();
+		}
+
+		transforms.Insert (0, new SyncTransform(position, rotation));
+	}
+}
