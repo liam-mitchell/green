432d882a71ff7f0293430648f6635c0d6e12bf5b
Liam Mitchell <ldmitche@ucalgary.ca>
2015-11-27 20:43:33 -0700

    Initial work on weapons components.

--- Assets/Scripts/Server/Weapon.cs
+++ Assets/Scripts/Server/Weapon.cs
@@ -1,32 +1,32 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
-using System.Collections;
-
-public class Weapon : NetworkBehaviour {
-	public float cooldown;
-	public Spawner projectileSpawner;
-	public TargetingSystem targetingSystem;
-
-	private PlayerInput input;
-	private float currentCooldown;
-	private GameObject lockTarget;
-
-	[ServerCallback]
-	void Start() {
-		input = GetComponent<PlayerInput>();
-	}
-
-	[ServerCallback]
-	void Update() {
-		if (input.state.fire && currentCooldown <= 0.0f) {
-			var projectile = projectileSpawner.Spawn (transform.position + transform.right * 0.5f,
-			                                          Quaternion.LookRotation(transform.right, transform.up));
-			var component = projectile.GetComponent<Projectile>();
-			component.Lock(targetingSystem.Target());
-			component.Ignore(gameObject);
-			currentCooldown = cooldown;
-		}
-
-		currentCooldown -= Time.deltaTime;
-	}
-}
+﻿//using UnityEngine;
+//using UnityEngine.Networking;
+//using System.Collections;
+//
+//public class Weapon : NetworkBehaviour {
+//	public float cooldown;
+//	public Spawner projectileSpawner;
+//	public TargetingSystem targetingSystem;
+//
+//	private PlayerInput input;
+//	private float currentCooldown;
+//	private GameObject lockTarget;
+//
+//	[ServerCallback]
+//	void Start() {
+//		input = GetComponent<PlayerInput>();
+//	}
+//
+//	[ServerCallback]
+//	void Update() {
+//		if (input.state.fire && currentCooldown <= 0.0f) {
+//			var projectile = projectileSpawner.Spawn (transform.position + transform.right * 0.5f,
+//			                                          Quaternion.LookRotation(transform.right, transform.up));
+//			var component = projectile.GetComponent<Projectile>();
+//			component.Lock(targetingSystem.Target());
+//			component.Ignore(gameObject);
+//			currentCooldown = cooldown;
+//		}
+//
+//		currentCooldown -= Time.deltaTime;
+//	}
+//}
---
+++ Assets/Scripts/Shared/ShipParts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿//using UnityEngine;
+//using UnityEngine.Networking;
+//using System.Collections;
+//
+//public class Weapon : ShipPart {
+//	public const string Name = "Weapon";
+//
+//
+//
+//	public static ShipPart _Deserialize(NetworkReader reader) {
+//
+//	}
+//
+//	public override void Serialize(NetworkWriter writer) {
+//
+//	}
+//
+//	public override int Mass() {
+//		return 1000;
+//	}
+//
+//	public override void Attach(Ship ship) {
+//
+//	}
+//
+//	public override void Detach(Ship ship) {
+//
+//	}
+//}
